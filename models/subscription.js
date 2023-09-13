277725614bde285dd21b449e0e84841ae50ad59d
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-13 15:15:37 +0500

    Update subscription.js

---
+++ models/subscription.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const subscriptionSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    channelName: {
+        type: String,
+        required: true
+    },
+    startDate: {
+        type: Date,
+        default: Date.now
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['active', 'expired'],
+        default: 'active'
+    }
+});
+
+module.exports = mongoose.model('Subscription', subscriptionSchema);
