237e45910d6fdc72916d934957cfee3661855530
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-13 15:47:34 +0500

    Update telegramBotIntegration.js

--- utils/telegramBotIntegration.js
+++ utils/telegramBotIntegration.js
@@ -1,11 +1,22 @@
+const TelegramBot = require('node-telegram-bot-api');
+const TOKEN = process.env.TELEGRAM_BOT_TOKEN; // Ensure this environment variable is set
+const bot = new TelegramBot(TOKEN, {polling: true});
+
+// Function to connect to the bot
 exports.connectToBot = () => {
-    // In a real-world scenario, you'd use the Telegram bot API or any relevant library to connect to the bot
     console.log('Connected to the Telegram bot');
+    // In a real-world scenario, you could add more initialization code here if needed
 };
 
-exports.sendMessage = (message) => {
-    // Logic to send a message to the Telegram bot using its API
-    console.log(`Sent message to the Telegram bot: ${message}`);
+// Function to send a message to a specific chat
+exports.sendMessage = (chatId, message) => {
+    bot.sendMessage(chatId, message)
+        .then(() => {
+            console.log(`Sent message to the Telegram chat ${chatId}: ${message}`);
+        })
+        .catch(err => {
+            console.error("Failed to send message:", err);
+        });
 };
 
-// Add other utility functions as needed...
+// You can add other utility functions related to the Telegram bot as needed...
