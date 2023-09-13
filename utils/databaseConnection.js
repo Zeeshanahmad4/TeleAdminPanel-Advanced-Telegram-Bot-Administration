8778e13074243039df0dda4df6d4eb5f60142801
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-13 15:34:30 +0500

    Update databaseConnection.js

---
+++ utils/databaseConnection.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/yourDatabaseName'; // Replace 'yourDatabaseName' with your actual DB name
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+        console.log("MongoDB connected successfully!");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1); // Exit process with failure code
+    }
+};
+
+module.exports = connectDB;
