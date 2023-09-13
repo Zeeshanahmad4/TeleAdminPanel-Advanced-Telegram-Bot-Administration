4fc11375477ba7e80c2b06acc144fde6bc4e7751
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-13 15:16:06 +0500

    Update payment.js

---
+++ models/payment.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const paymentSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    contentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Content',
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: String,
+        enum: ['completed', 'pending', 'failed'],
+        default: 'pending'
+    }
+});
+
+module.exports = mongoose.model('Payment', paymentSchema);
