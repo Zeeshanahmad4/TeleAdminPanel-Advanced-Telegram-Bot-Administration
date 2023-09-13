ebf025f7aba9e0baa088a9143e673765658cf367
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-13 14:57:17 +0500

    Update telegramBotIntegration.js

---
+++ utils/telegramBotIntegration.js
@@ -0,0 +1,11 @@
+exports.connectToBot = () => {
+    // In a real-world scenario, you'd use the Telegram bot API or any relevant library to connect to the bot
+    console.log('Connected to the Telegram bot');
+};
+
+exports.sendMessage = (message) => {
+    // Logic to send a message to the Telegram bot using its API
+    console.log(`Sent message to the Telegram bot: ${message}`);
+};
+
+// Add other utility functions as needed...
