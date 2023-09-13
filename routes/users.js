b2f5b25624f7ff39fd9281c9fc794c497ac05ccf
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-13 15:32:19 +0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/usersController');
 
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    // ... other user fields
-});
+// Route for user registration
+router.post('/register', usersController.register);
 
-module.exports = mongoose.model('User', userSchema);
+// Route for user login
+router.post('/login', usersController.login);
+
+// Route for user logout
+router.get('/logout', usersController.logout);
+
+// Route to fetch details of a specific user by their username
+router.get('/:username', usersController.getUserDetails);
+
+// Route to update user details
+router.put('/update/:userId', usersController.updateUser);
+
+// Route to delete a user account
+router.delete('/delete/:userId', usersController.deleteUser);
+
+module.exports = router;
