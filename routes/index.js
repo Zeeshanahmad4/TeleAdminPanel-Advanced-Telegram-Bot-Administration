9b1287b3e2cd46fbd2c4839ad24c7edd5481327c
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-13 16:01:36 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('dashboard');  // Assuming you have a dashboard view
+    res.render('dashboard');  
 });
 
 module.exports = router;
